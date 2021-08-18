14bcaae90cef471aedb7275cf60c4d64b905beaf
Adeyemi Mewayewon <4verprime@gmail.com>
2021-08-18 01:18:53 +0100

    working

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+var tpl *template.Template
+
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/process", processor)
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
+	http.ListenAndServe(":80", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	name := Name{"Adeyemi Mewayewon", "Adeyemi Mewayewon"}
+	template, _ := template.ParseFiles("index.html")
+	template.Execute(w, name)
+}
+
+type Name struct {
+	FName, LName string
+}
+
+func processor(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	fname := r.FormValue("user")
+	lname := r.FormValue("email")
+	sname := r.FormValue("subject")
+	mname := r.FormValue("message")
+
+	d := struct {
+		User    string
+		Email   string
+		Subject string
+		Message string
+	}{
+		User:    fname,
+		Email:   lname,
+		Subject: sname,
+		Message: mname,
+	}
+	tpl.ExecuteTemplate(w, "processor.html", d)
+}
